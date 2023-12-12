9603c1f7be1df7954a62f943c2c6aaaba43f3b3c
AJ Chau <chaujannette@gmail.com>
2023-12-11 21:11:11 -0500

    working, wihtout load balancing

--- trackerserver.py
+++ trackerserver.py
@@ -26,65 +26,65 @@ def find_lines_with_word(filename, word):
         newList.append([line.strip().split(":")[1].replace('"', '').replace("'", "").replace("\\", "").replace("}", "").replace("]", "").replace("[", "").replace("{", "").replace(" ", "") for line in lines if word in line])
         return newList
 
-def read_ip_counts():
-  ip_counts = {}
-  try:
-    with open("ip_counts.txt") as f:
-      for line in f:
-        # Split the line and remove unnecessary characters
-        key, value = line.strip().split(",")
-        key = key.replace("'", "").replace(":", "")
+# def read_ip_counts():
+#   ip_counts = {}
+#   try:
+#     with open("ip_counts.txt") as f:
+#       for line in f:
+#         # Split the line and remove unnecessary characters
+#         key, value = line.strip().split(",")
+#         key = key.replace("'", "").replace(":", "")
 
-        # Handle None values and convert to integer
-        try:
-          value = int(value)
-        except ValueError:
-          value = 0
+#         # Handle None values and convert to integer
+#         try:
+#           value = int(value)
+#         except ValueError:
+#           value = 0
 
-        ip_counts[key] = value
-  except FileNotFoundError:
-    pass
-  return ip_counts
+#         ip_counts[key] = value
+#   except FileNotFoundError:
+#     pass
+#   return ip_counts
 
-ip_counts = read_ip_counts()
-print(ip_counts)  # Output: {'127.0.0.1': 0}
+# ip_counts = read_ip_counts()
+# print(ip_counts)  # Output: {'127.0.0.1': 0}
 
-def write_ip_counts(ip_counts):
-  with open(IP_COUNT_FILE, "w") as f:
-    for ip, count in ip_counts.items():
-      # Write the IP address and count in the desired format
-      f.write(f"{ip}:{count}\n")
+# def write_ip_counts(ip_counts):
+#   with open(IP_COUNT_FILE, "w") as f:
+#     for ip, count in ip_counts.items():
+#       # Write the IP address and count in the desired format
+#       f.write(f"{ip}:{count}\n")
 
-def get_least_count_ip(ip_counts, new_ips):
-# Filter the list to include only IPs with counts
-  filtered_ips = [ip for ip in new_ips if ip in ip_counts]
+# def get_least_count_ip(ip_counts, new_ips):
+# # Filter the list to include only IPs with counts
+#   filtered_ips = [ip for ip in new_ips if ip in ip_counts]
 
-  # Find the IP with the least count
-  if not filtered_ips:
-    return random.choice(new_ips)
-  else: 
-    min_count = min(ip_counts.values() for ip in filtered_ips)
-    min_count_ips = [ip for ip in filtered_ips if ip_counts[ip] == min_count]
+#   # Find the IP with the least count
+#   if not filtered_ips:
+#     return random.choice(new_ips)
+#   else: 
+#     min_count = min(ip_counts.values() for ip in filtered_ips)
+#     min_count_ips = [ip for ip in filtered_ips if ip_counts[ip] == min_count]
 
-    # If there are multiple IPs with the least count, select one randomly
-    if len(min_count_ips) > 1:
-      selected_ip = random.choice(min_count_ips)
-    else:
-      selected_ip = min_count_ips[0]
+#     # If there are multiple IPs with the least count, select one randomly
+#     if len(min_count_ips) > 1:
+#       selected_ip = random.choice(min_count_ips)
+#     else:
+#       selected_ip = min_count_ips[0]
 
-    return selected_ip
+#     return selected_ip
 
-def update_ip_count(ip_set, ip):
-  # Convert to string for consistent comparison
-  ip = str(ip)
-  if ip in ip_set:
-    ip_set.pop(ip)
-    ip_set.setdefault(f"{ip}:1")  # Add count to the IP address
-  else:
-    ip_set.setdefault(f"{ip}:0")  # Add the new IP with a count of 0
+# def update_ip_count(ip_set, ip):
+#   # Convert to string for consistent comparison
+#   ip = str(ip)
+#   if ip in ip_set:
+#     ip_set.pop(ip)
+#     ip_set.setdefault(f"{ip}:1")  # Add count to the IP address
+#   else:
+#     ip_set.setdefault(f"{ip}:0")  # Add the new IP with a count of 0
 
-  print(ip_set)
-  return ip_set
+#   print(ip_set)
+#   return ip_set
 
 
 
@@ -95,7 +95,6 @@ while True:
     print(f"New connection from {addr}")
     ipAdd = str({addr})
 
-    # Receive the peer ID and the file list from the client
     ipAddress = conn.recv(1024).decode()
     requestedFile = conn.recv(1024).decode()
 
@@ -118,16 +117,16 @@ while True:
         # update_ip_request_count(chosenIP)
         # print(newList)
         # print(chosenIP)
-        ipCounts = read_ip_counts()
-        print(ipCounts)
-        test = get_least_count_ip(ipCounts, returnList)
-        print(test)
-        ipCounts = update_ip_count(ipCounts, test)
-        print(ipCounts)
-        write_ip_counts(ipCounts)
+        # ipCounts = read_ip_counts()
+        # print(ipCounts)
+        # test = get_least_count_ip(ipCounts, returnList)
+        # print(test)
+        # ipCounts = update_ip_count(ipCounts, test)
+        # print(ipCounts)
+        # write_ip_counts(ipCounts)
 
-        print(test)
-        print(newList)
+        # print(test)
+        # print(newList)
         #print(text)
 
     returnValue = str(returnList[0])
