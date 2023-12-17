adab1c41972a24b41bcaf25033a080229aee70ab
marycalabro <125496396+marycalabro@users.noreply.github.com>
2023-12-17 15:14:23 -0500

    Update trackerserver.py
    adding load balancer

--- trackerserver.py
+++ trackerserver.py
@@ -1,17 +1,12 @@
+# Import socket module
 import socket
 import json
 from collections import defaultdict
 import random
+import re
 
+requestedFile = ""
 IP_COUNT_FILE = "ip_counts.txt"
-FILES_FILE = "files.txt"
-
-# Dictionary for files and their owners
-files_dict = defaultdict(lambda: defaultdict(int))
-
-# Dictionary for IP counts
-ip_counts = defaultdict(lambda: defaultdict(int))
-
 # Create a socket object
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -21,70 +16,148 @@ server.bind(('0.0.0.0', 8080))
 # Listen for incoming connections
 server.listen(5)
 
-def read_files_dict():
-    files_dict = defaultdict(lambda: defaultdict(int))
-    try:
-        with open(FILES_FILE) as f:
-            for line in f:
-                file, ip = line.strip().split(":")
-                files_dict[file][ip] = files_dict[file].get(ip, 0) + 1
-    except FileNotFoundError:
-        pass
-    return files_dict
+# A dictionary to store the files and their owners
+files = {}
 
-def write_files_dict(files_dict):
-    with open(FILES_FILE, "w") as f:
-        for file, ip_dict in files_dict.items():
-            for ip, count in ip_dict.items():
-                f.write(f"{file}:{ip}\n")
+#A dictionary of filenames & # of times a certain IP has sent it
+ip_count = {
+    "t.txt": {
+       "172.23.100.23123": 0,
+        "172.23.100.677": 0,
+        "172.23": 0
+    },  
+}
+def loadBalancing(requestedFile):
+	#setting the first node of the requested txt file to the first node as a 
+    #placeholder
+    ipSend = list(ip_count.get(requestedFile).keys())[0]
+    
+    nodeCount = 0
+    
+    #length of nested dict
+    numDicts = len(ip_count.get(requestedFile).keys())
+    for node in ips.get(requestedFile):
+        if(ip_count[requestedFile][node] < ip_count[requestedFile][ipSend]):
+            ipSend = node
+        nodeCount+=1
+    return ipSend
+   
 
-def read_ip_counts():
-    ip_counts = defaultdict(lambda: defaultdict(int))
-    try:
-        with open(IP_COUNT_FILE) as f:
-            for line in f:
-                ip, file, count = line.strip().split(":")
-                ip_counts[ip][file] = int(count)
-    except FileNotFoundError:
-        pass
-    return ip_counts
 
-def write_ip_counts(ip_counts):
-    with open(IP_COUNT_FILE, "w") as f:
-        for ip, file_counts in ip_counts.items():
-            for file, count in file_counts.items():
-                f.write(f"{ip}: {file}: {count}\n")
+def find_lines_with_word(filename, word):
+    newList = []
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        newList.append([line.strip().split(":")[1].replace('"', '').replace("'", "").replace("\\", "").replace("}", "").replace("]", "").replace("[", "").replace("{", "").replace(" ", "") for line in lines if word in line])
+        return newList
 
-def choose_valid_least_requested_ip(ip_dict):
-    # logic for choosing the least requested IP
-    return min(ip_dict, key=ip_dict.get)
+# def read_ip_counts():
+#   ip_counts = {}
+#   try:
+#     with open("ip_counts.txt") as f:
+#       for line in f:
+#         # Split the line and remove unnecessary characters
+#         key, value = line.strip().split(",")
+#         key = key.replace("'", "").replace(":", "")
+
+#         # Handle None values and convert to integer
+#         try:
+#           value = int(value)
+#         except ValueError:
+#           value = 0
+
+#         ip_counts[key] = value
+#   except FileNotFoundError:
+#     pass
+#   return ip_counts
+
+# ip_counts = read_ip_counts()
+# print(ip_counts)  # Output: {'127.0.0.1': 0}
+
+# def write_ip_counts(ip_counts):
+#   with open(IP_COUNT_FILE, "w") as f:
+#     for ip, count in ip_counts.items():
+#       # Write the IP address and count in the desired format
+#       f.write(f"{ip}:{count}\n")
+
+# def get_least_count_ip(ip_counts, new_ips):
+# # Filter the list to include only IPs with counts
+#   filtered_ips = [ip for ip in new_ips if ip in ip_counts]
+
+#   # Find the IP with the least count
+#   if not filtered_ips:
+#     return random.choice(new_ips)
+#   else: 
+#     min_count = min(ip_counts.values() for ip in filtered_ips)
+#     min_count_ips = [ip for ip in filtered_ips if ip_counts[ip] == min_count]
+
+#     # If there are multiple IPs with the least count, select one randomly
+#     if len(min_count_ips) > 1:
+#       selected_ip = random.choice(min_count_ips)
+#     else:
+#       selected_ip = min_count_ips[0]
+
+#     return selected_ip
+
+# def update_ip_count(ip_set, ip):
+#   # Convert to string for consistent comparison
+#   ip = str(ip)
+#   if ip in ip_set:
+#     ip_set.pop(ip)
+#     ip_set.setdefault(f"{ip}:1")  # Add count to the IP address
+#   else:
+#     ip_set.setdefault(f"{ip}:0")  # Add the new IP with a count of 0
+
+#   print(ip_set)
+#   return ip_set
+
+
 
 while True:
     # Accept a connection
     conn, addr = server.accept()
+    fileName = conn.recv(1024).decode()
     print(f"New connection from {addr}")
+    ipAdd = str({addr})
 
-    # Receive data from the client
     ipAddress = conn.recv(1024).decode()
     requestedFile = conn.recv(1024).decode()
 
-    # Update files dictionary
-    files_dict[requestedFile][ipAddress] = files_dict[requestedFile].get(ipAddress, 0) + 1
+    # update_ip_request_count(ipAdd)
+    # Add the peer and its address to the dictionary
 
-    # Choose the least requested IP
-    chosenIP = choose_valid_least_requested_ip(files_dict[requestedFile])
+    # Add the files and their owners to the dictionary
+    for file in fileName.split(","):
+        with open("files.txt", "a") as f:
+            f.write(str(file) + ":" + str(ipAddress))
+            f.write("\n")
 
-    # Send the chosen IP back to the client
-    conn.send(chosenIP.encode('utf-8'))
 
-    # Update IP count
-    ip_counts[ipAddress][requestedFile] += 1
+    if requestedFile != "":
+        newList = []
+        newList = find_lines_with_word("files.txt", requestedFile)
+        newList = str(newList)
+        returnList = list(newList.split(","))
+        # chosenIP = choose_valid_least_requested_ip(newList)
+        # update_ip_request_count(chosenIP)
+        # print(newList)
+        # print(chosenIP)
+        # ipCounts = read_ip_counts()
+        # print(ipCounts)
+        # test = get_least_count_ip(ipCounts, returnList)
+        # print(test)
+        # ipCounts = update_ip_count(ipCounts, test)
+        # print(ipCounts)
+        # write_ip_counts(ipCounts)
 
-    print(f"Chosen IP: {chosenIP}")
+        # print(test)
+        # print(newList)
+        #print(text)
+
+    returnValue = str(returnList[0])
+    returnValue = returnValue.replace('"', '').replace("'", "").replace("\\", "").replace("}", "").replace("]", "").replace("[", "").replace("{", "").replace(" ", "")
+    # Send the list of peers and files to the client
+    conn.send(returnValue.encode('utf-8'))
 
     # Close the connection
     conn.close()
-
-    # Write dictionaries to files
-    write_files_dict(files_dict)
-    write_ip_counts(ip_counts)
